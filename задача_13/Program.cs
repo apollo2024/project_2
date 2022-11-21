2b5c2a1682ca9b0e90a2121c1cb19300dd424e84
muhammadkodir <muhammadkodyra@email.com>
2022-11-21 22:36:46 +0500

    c1

---
+++ задача_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("number:");
+var number = Convert.ToInt32(Console.ReadLine());
+if (number > 999 || number < 100)
+{
+    Console.WriteLine("число не трехзначное");
+}
+if (number.Length > 3);
+{
+    var i = number.Length;
+    for (i > 3) continue;
+}
+
+
+
