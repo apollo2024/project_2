e972300296ac29b19991c39f40816f4a7cc862ad
muhammadkodir <muhammadkodyra@email.com>
2022-11-22 00:10:37 +0500

    i hope this is the last one

--- задача_13/Program.cs
+++ задача_13/Program.cs
@@ -1,21 +1,20 @@
 ﻿Console.WriteLine("number:");
-var number = Convert.ToInt32(Console.ReadLine());
-int m = 10;
+var number = Convert.ToInt64(Console.ReadLine());
 if (number < 100)
 {
-    Console.WriteLine("число не трехзначное");
+    Console.WriteLine("не трехзначное");
 }
+else
 if ( number > 999)
 {
-    for (int i = 999; number > i; m = m * 10)
-    {
-         int super_number = (number / m);
-         int sum = (super_number / 10) - (super_number / 100) * 10 ;
-         Console.WriteLine(sum);
-    }
+    var m = 10;
+    var n = number;
+    for (int i = 999; n > i;n = n / m);
+    var sum = n - (n / 10) * 10;
+     Console.WriteLine(sum);
 }
 else
 {
-    int sum = (number / 10) - (number / 100) * 10 ;
+    var sum = number - (number / 10) * 10;
     Console.WriteLine(sum);
 }
