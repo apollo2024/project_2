ff7a976ebfc59ceb12b0babb9e9e785dd701bec1
muhammadkodir <muhammadkodyra@email.com>
2022-11-22 00:47:25 +0500

    the last one

---
+++ задача_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("put date of the day:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0 || number > 31)
+{
+    Console.WriteLine("your number is not a day");
+}
+else
+if (number %7 == 0 || number %6 == 0)
+{
+    Console.WriteLine("this day is a holiday");
+}
+else 
+{
+    Console.WriteLine("this day is not a holiday");
+}
